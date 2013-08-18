6103598736231c83e6bc29bfae7769f3a5b783cd
Tessa Lau <tlau@tlau.org>
2013-08-18 14:33:22 -0700

    Added info about account logins

--- server/startup.js
+++ server/startup.js
@@ -1,3 +1,24 @@
 Meteor.startup(function () {
   // code to run on server at startup
+
+  // Add details about login services if not already there
+  // Note these are for an app running on localhost; we'll need to change
+  // them for deployment
+  if (!Accounts.loginServiceConfiguration.findOne({service: 'google'})) {
+    Accounts.insert({
+      service: 'google',
+      clientId: '74664641477.apps.googleusercontent.com',
+      secret : 'giJls9204uwHUwYgAuXoGR8-',
+      _id : 'cECfzjat4CArnLMhw'
+    });
+  }
+
+  if (!Accounts.loginServiceConfiguration.findOne({service: 'facebook'})) {
+    Accounts.insert({
+      service: 'facebook',
+      appId : '227251324090788',
+      secret : '3b30264f85ca3df3b742d28c092d52e8',
+      _id : '8MtnYh9MiJdCdvgEd'
+    });
+  }
 });
