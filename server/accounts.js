050bf3a02b25cd6794de79133d0c40036cdeb008
Julian Cerruti <jcerruti@willowgarage.com>
2013-08-27 09:13:43 -0300

    Updating to acounts-persona without need of workaround

--- server/accounts.js
+++ server/accounts.js
@@ -1,5 +1,7 @@
 Accounts.onCreateUser(function(options, user) {
   user.profile = options.profile || {};
-  user.profile.email = user.services.facebook.email;
+  user.profile.email = user.services.persona.email;
+  user.username = user.profile.email;
+  user.email = user.profile.email;
   return user;
 });
